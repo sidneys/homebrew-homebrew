0974d794761f865525a9abc855e5fdff4bbc42ba
sidneys <sidneys.github.io@outlook.com>
2019-02-11 22:48:23 +0100

    add --enable-nvenc

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -109,6 +109,12 @@ class Ffmpeg < Formula
       --enable-lzma
     ]
 
+    # NVIDIA Hardware Acceleration
+    args << "--enable-nvenc"
+    # args << "--enable-cuda"
+    # args << "--enable-cuvid"
+    # args << "--enable-libnpp"
+
     args << "--enable-chromaprint" if build.with? "chromaprint"
     args << "--enable-frei0r" if build.with? "frei0r"
     args << "--enable-libaom" if build.with? "aom"
