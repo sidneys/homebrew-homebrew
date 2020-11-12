0f579f10b68fdf74ffe0dcc10ed33dfc67c75ea8
sidneys <sidneys.github.io@outlook.com>
2020-11-12 23:50:54 +0100

    feat: add youtube-dl, youtube-dlc

---
+++ Formula/youtube-dl.rb
@@ -0,0 +1,38 @@
+class YoutubeDl < Formula
+  desc "Download YouTube videos from the command-line"
+  homepage "https://youtube-dl.org/"
+  url "https://gitlab.com/dstftw/youtube-dl/uploads/99d745f22ca3c2a8e9a23def5446289a/youtube-dl-2020.11.12.tar.gz"
+  sha256 "aeb00b2079c4711de7eea2a91f6923ebade84c5e985b0caeb47d572395d42831"
+  license "Unlicense"
+
+  head do
+    url "https://gitlab.com/sidneys/youtube-dl.git"
+    depends_on "pandoc" => :build
+  end
+
+  bottle :unneeded
+
+  option "with-testing", "deploy/testing branch"
+
+  if build.with? "testing"
+    head do
+      url "https://gitlab.com/sidneys/youtube-dl.git", branch: "deploy/testing"
+    end
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}" if build.head?
+    bin.install "youtube-dl"
+    man1.install "youtube-dl.1"
+    bash_completion.install "youtube-dl.bash-completion"
+    zsh_completion.install "youtube-dl.zsh" => "_youtube-dl"
+    fish_completion.install "youtube-dl.fish"
+  end
+
+  test do
+    # commit history of homebrew-core repo
+    system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
+    # homebrew playlist
+    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
+  end
+end
---
+++ Formula/youtube-dlc.rb
@@ -0,0 +1,28 @@
+class YoutubeDlc < Formula
+  desc "Fork of youtube-dl with the intention of getting features tested by the community merged in the tool faster"
+  homepage "https://github.com/blackjack4494/yt-dlc"
+  license "Unlicense"
+
+  head do
+    url "https://github.com/blackjack4494/yt-dlc.git"
+    depends_on "pandoc" => :build
+  end
+
+  bottle :unneeded
+
+  def install
+    system "make", "PREFIX=#{prefix}" if build.head?
+    bin.install "youtube-dlc"
+    man1.install "youtube-dlc.1"
+    bash_completion.install "youtube-dlc.bash-completion"
+    zsh_completion.install "youtube-dlc.zsh" => "_youtube-dlc"
+    fish_completion.install "youtube-dlc.fish"
+  end
+
+  test do
+    # commit history of homebrew-core repo
+    system "#{bin}/youtube-dlc", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
+    # homebrew playlist
+    system "#{bin}/youtube-dlc", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
+  end
+end
