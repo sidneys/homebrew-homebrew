677d5ef10064110b17fcd7c9e7df2a24ae053cf8
sidneys <sidneys.github.io@outlook.com>
2021-09-15 23:33:00 +0200

    upgrade: shairport-sync, pulseaudio

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -1,9 +1,9 @@
 class Pulseaudio < Formula
   desc "Sound system for POSIX OSes"
   homepage "https://wiki.freedesktop.org/www/Software/PulseAudio/"
-  url "https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-13.0.tar.xz"
-  sha256 "961b23ca1acfd28f2bc87414c27bb40e12436efcf2158d29721b1e89f3f28057"
-  revision 1
+  url "https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-14.2.tar.xz"
+  sha256 "75d3f7742c1ae449049a4c88900e454b8b350ecaa8c544f3488a2562a9ff66f1"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later", "BSD-3-Clause"]
 
   # The regex here avoids x.99 releases, as they're pre-release versions.
   livecheck do
@@ -12,9 +12,11 @@ class Pulseaudio < Formula
   end
 
   bottle do
-    sha256 catalina:    "0e9445dd8d49abd299324e93f00231605e993f791674997d9d2c35b88efec528"
-    sha256 mojave:      "ae68dfdb8ad584bf3f602ea7fb36d9bc1e4540e6905986a7129e45c6170d8d95"
-    sha256 high_sierra: "687c4c646487eb8a9988303e279dc2ee542b6404504cb54fcfce1d6d6bcf949f"
+    sha256 arm64_big_sur: "efcbf144da932e05394e9768bf27dfa1908dbb17f4b7c52f49e56c791dd51860"
+    sha256 big_sur:       "79684acaac85e9b1b7de55fc7659844d9508c6264faa0aac311e0d8eaf4056b0"
+    sha256 catalina:      "e1c181ae27f945ceee403e2e2ec80f44aebd52ac44b8e63140c1c9d2083a643b"
+    sha256 mojave:        "ae0d2ec72fc10a895c7efc330174abef08458576ed847fb4547301a2d8cc147e"
+    sha256 x86_64_linux:  "35c1358237eefe762c268cbbbf86015b425e8ff3bdff697afb93e8449fae2ae3"
   end
 
   head do
@@ -38,18 +40,54 @@ class Pulseaudio < Formula
   uses_from_macos "expat"
   uses_from_macos "m4"
 
+  on_linux do
+    depends_on "dbus"
+    depends_on "glib"
+    depends_on "libcap"
+
+    resource "XML::Parser" do
+      url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.44.tar.gz"
+      sha256 "1ae9d07ee9c35326b3d9aad56eae71a6730a73a116b9fe9e8a4758b7cc033216"
+    end
+  end
+
   def install
+    if OS.linux?
+      ENV.prepend_create_path "PERL5LIB", buildpath/"lib/perl5"
+      resource("XML::Parser").stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{buildpath}"
+        system "make", "PERL5LIB=#{ENV["PERL5LIB"]}", "CC=#{ENV.cc}"
+        system "make", "install"
+      end
+    end
+
     args = %W[
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
-      --enable-coreaudio-output
       --disable-neon-opt
       --disable-nls
       --disable-x11
-      --with-mac-sysroot=#{MacOS.sdk_path}
-      --with-mac-version-min=#{MacOS.version}
     ]
+
+    if OS.mac?
+      args << "--enable-coreaudio-output"
+      args << "--with-mac-sysroot=#{MacOS.sdk_path}"
+      args << "--with-mac-version-min=#{MacOS.version}"
+    else
+      # Perl depends on gdbm.
+      # If the dependency of pulseaudio on perl is build-time only,
+      # pulseaudio detects and links gdbm at build-time, but cannot locate it at run-time.
+      # Thus, we have to
+      #  - specify not to use gdbm, or
+      #  - add a dependency on gdbm if gdbm is wanted (not implemented).
+      # See Linuxbrew/homebrew-core#8148
+      args << "--with-database=simple"
+
+      # Tell pulseaudio to use the brewed udev rules dir instead of the system one,
+      # which it does not have permission to modify
+      args << "--with-udev-rules-dir=#{lib}/udev/rules.d"
+    end
 
     if build.head?
       # autogen.sh runs bootstrap.sh then ./configure
@@ -58,35 +96,19 @@ class Pulseaudio < Formula
       system "./configure", *args
     end
     system "make", "install"
+
+    if OS.linux?
+      # https://stackoverflow.com/questions/56309056/is-gschemas-compiled-architecture-specific-can-i-ship-it-with-my-python-library
+      rm "#{share}/glib-2.0/schemas/gschemas.compiled"
+    end
   end
 
-  plist_options manual: "pulseaudio"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/pulseaudio</string>
-          <string>--exit-idle-time=-1</string>
-          <string>--verbose</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/#{name}.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/#{name}.log</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"pulseaudio", "--exit-idle-time=-1", "--verbose"]
+    keep_alive true
+    log_path var/"log/pulseaudio.log"
+    error_log_path var/"log/pulseaudio.log"
+    run_type :immediate
   end
 
   test do
--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -1,15 +1,31 @@
 class ShairportSync < Formula
   desc "AirTunes emulator that adds multi-room capability"
   homepage "https://github.com/mikebrady/shairport-sync"
-  url "https://github.com/mikebrady/shairport-sync/archive/3.3.2.tar.gz"
-  sha256 "a8f580fa8eb71172f6237c0cdbf23287b27f41f5399f5addf8cd0115a47a4b2b"
+  url "https://github.com/mikebrady/shairport-sync/archive/3.3.8.tar.gz"
+  sha256 "c92f9a2d86dd1138673abc66e0010c94412ad6a46da8f36c3d538f4fa6b9faca"
+  license "MIT"
   head "https://github.com/mikebrady/shairport-sync.git", :branch => "development"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    rebuild 2
+    sha256 arm64_big_sur: "5ba3c8e5edb375fc0704715f2af591a95bccc371d30f19d6af77b27069de80c5"
+    sha256 big_sur:       "f901fdd19cf7effc603519cc071703014c52264434d6da2eac51da37323a12ce"
+    sha256 catalina:      "f7e414e4106c07a4ec3db03f1d0219280d47eedbef5c65f30ac3c488e5978cd4"
+    sha256 mojave:        "7258fbd051a45b0fd5524b1017c6435be4471c4ccaef63b4c733c2ed2efb31ac"
+    sha256 x86_64_linux:  "0502a7c91fe4f656ce22633c52de830d1f7a783b31af0740234f808d0ce16dde"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libao"
   depends_on "libconfig"
+  depends_on "libdaemon"
   depends_on "libsoxr"
   depends_on "openssl@1.1"
   depends_on "popt"
@@ -21,9 +37,8 @@ class ShairportSync < Formula
     args = %W[
       --with-ao
       --with-apple-alac
-      --with-dns_sd
+      --with-libdaemon
       --with-metadata
-      --with-os=darwin
       --with-pa
       --with-pipe
       --with-soxr
@@ -33,6 +48,10 @@ class ShairportSync < Formula
       --sysconfdir=#{etc}/shairport-sync
       --prefix=#{prefix}
     ]
+    if OS.mac?
+      args << "--with-dns_sd" # Enable bonjour
+      args << "--with-os=darwin"
+    end
     system "./configure", *args
     system "make", "install"
   end
@@ -41,31 +60,22 @@ class ShairportSync < Formula
     (var/"run").mkpath
   end
 
-  plist_options :manual => "shairport-sync"
+  service do
+    run [opt_bin/"shairport-sync", "--use-stderr", "--verbose"]
+    keep_alive true
+    log_path var/"log/shairport-sync.log"
+    error_log_path var/"log/shairport-sync.log"
+    run_type :immediate
+  end
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/shairport-sync</string>
-        <string>--use-stderr</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/#{name}.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/#{name}.log</string>
-    </dict>
-    </plist>
-  EOS
+  test do
+    output = shell_output("#{bin}/shairport-sync -V")
+    on_macos do
+      assert_match "libdaemon-OpenSSL-dns_sd-ao-pa-stdout-pipe-soxr-metadata", output
+    end
+    on_linux do
+      assert_match "OpenSSL-ao-pa-stdout-pipe-soxr-metadata-sysconfdir", output
+    end
   end
 
   def caveats
@@ -76,9 +86,5 @@ class ShairportSync < Formula
       `pulseaudio` has already been installed as a Homebrew dependency. To run it unobstrusively in
       the background (recommended), start it as a service: `brew services start pulseaudio`
     EOS
-  end
-
-  test do
-    assert_match "libdaemon-OpenSSL-dns_sd-ao-pa-stdout-pipe-soxr-convolution-metadata", shell_output("#{bin}/shairport-sync -V")
   end
 end
