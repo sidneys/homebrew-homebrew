bc2d3f6d626709ac48b8edcd0506b508d3b5595a
sidneys <sidneys.github.io@outlook.com>
2022-06-06 22:55:48 +0200

    update: megatools-experimental

--- Formula/megatools-experimental.rb
+++ Formula/megatools-experimental.rb
@@ -1,13 +1,14 @@
 class MegatoolsExperimental < Formula
   desc "Command-line client for Mega.co.nz"
   homepage "https://megatools.megous.com/"
-  url "https://megatools.megous.com/builds/experimental/megatools-1.11.0-git-20211030.tar.gz"
-  sha256 "35dd8b3c349ad1c8652b5df8bc69bb44f7455f329bfffc8a51a24db4cd5f99a4"
-  license "GPL-2.0-only"
+  url "https://megatools.megous.com/builds/experimental/megatools-1.11.0-git-20220401.tar.gz"
+  sha256 "e63fc192c69cb51436beff95940b69e843a0e82314251d28e48e9388c374b3f1"
+  license "GPL-2.0-or-later"
+  head "https://megatools.megous.com/git/megatools", branch: "master"
 
   livecheck do
     url "https://megatools.megous.com/builds/experimental/"
-    regex(/href=.*?megatools[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    regex(/href=.*?megatools[._-]v?(\d+(?:\.\d+)+)-git-\d{8}\.t/i)
   end
 
   depends_on "meson" => :build
