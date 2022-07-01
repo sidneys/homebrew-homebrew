dcde3532d3d24ca0c013ef812a4f45716fb85801
sidneys <sidneys.github.io@outlook.com>
2022-07-01 06:37:16 +0200

    feat: deploy yt-dlp, youtube-dl HEAD versions via python virtualenv also

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -7,15 +7,6 @@ class YoutubeDl < Formula
   sha256 "bc59e86c5d15d887ac590454511f08ce2c47698d5a82c27bfe27b5d814bbaed2"
   license "Unlicense"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "79bc7ca875b43074c1a7097674b900835c5358e80d81a177b1eecf0e69b77a7a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "79bc7ca875b43074c1a7097674b900835c5358e80d81a177b1eecf0e69b77a7a"
-    sha256 cellar: :any_skip_relocation, monterey:       "a8da6929ac2005f3bf5e004ccd1f860c6368cc49ea4d4846a0b902d21cc0cb7b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a8da6929ac2005f3bf5e004ccd1f860c6368cc49ea4d4846a0b902d21cc0cb7b"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8da6929ac2005f3bf5e004ccd1f860c6368cc49ea4d4846a0b902d21cc0cb7b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d96e5082a83cd8d333f72f40592ff577a4c4ccf4e654ddf2c3e38fccbb5c1f9"
-  end
-
   head do
     url "https://bitbucket.org/sidneys/youtube-dl.git", branch: "feature-merges/youtube-dl"
     depends_on "pandoc" => :build
@@ -24,19 +15,11 @@ class YoutubeDl < Formula
   depends_on "python@3.10"
 
   def install
-    if build.head?
-      system "make", "all", "PREFIX=#{prefix}"
-      bin.install "youtube-dl"
-      man1.install "youtube-dl.1"
-      bash_completion.install "youtube-dl.bash-completion"
-      zsh_completion.install "youtube-dl.zsh" => "_youtube-dl"
-      fish_completion.install "youtube-dl.fish"
-    else
-      virtualenv_install_with_resources
-      man1.install_symlink libexec/"share/man/man1/youtube-dl.1" => "youtube-dl.1"
-      bash_completion.install libexec/"etc/bash_completion.d/youtube-dl.bash-completion"
-      fish_completion.install libexec/"etc/fish/completions/youtube-dl.fish"
-    end
+    system "make", "all" if build.head?
+    virtualenv_install_with_resources
+    man1.install_symlink libexec/"share/man/man1/youtube-dl.1" => "youtube-dl.1"
+    bash_completion.install libexec/"etc/bash_completion.d/youtube-dl.bash-completion"
+    fish_completion.install libexec/"etc/fish/completions/youtube-dl.fish"
   end
 
   test do
@@ -46,3 +29,4 @@ class YoutubeDl < Formula
     system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
   end
 end
+
--- Formula/yt-dlp.rb
+++ Formula/yt-dlp.rb
@@ -3,18 +3,9 @@ class YtDlp < Formula
 
   desc "Fork of youtube-dl with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://files.pythonhosted.org/packages/5c/e0/e5bae3e87ee6da0f7507f3b58c5e9ffc1500de0742886ccc72c1a56740f2/yt-dlp-2022.2.4.tar.gz"
-  sha256 "81b50ed7cf9cfcc042d8f5a1ad2d1cd7b13c48b36c07faf1880696eac0a7ddb5"
+  url "https://files.pythonhosted.org/packages/4d/63/c2280d3cc56c6545e348cbfb589a698e211d967c666b4682adfd880318dc/yt-dlp-2022.6.22.1.tar.gz"
+  sha256 "ee401a9dcc7e9285b14f13229c3dcefdf387e597f4f4f773dab326aafe3b830c"
   license "Unlicense"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ca50f2157bec68534c4f4188e6398830a7c2a73dbf5ed78e579760f1197d8704"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7f0ac91b719b7dce2489169393c65c764af5a5c1f7e28fcdf51b408501655995"
-    sha256 cellar: :any_skip_relocation, monterey:       "24b775f99c550fd3ae4c408c735bee97368ab3b26ab9572ed134175b1cc27d95"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d1090c7956a99aaf339272465dc9605855cf0adb0a54daceac0af7e6baab5c40"
-    sha256 cellar: :any_skip_relocation, catalina:       "7c87b4e0cf50dc631b8b3460c54a2ff1f22821408b7373c894c7a84846ee827f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee0dd20f1e980a2d05a85e586e11be0c62780623caa96670b0b2c16969953f23"
-  end
 
   head do
     url "https://bitbucket.org/sidneys/yt-dlp.git", branch: "feature-merges/yt-dlp"
@@ -23,36 +14,38 @@ class YtDlp < Formula
 
   depends_on "python@3.10"
 
+  resource "Brotli" do
+    url "https://files.pythonhosted.org/packages/2a/18/70c32fe9357f3eea18598b23aa9ed29b1711c3001835f7cf99a9818985d0/Brotli-1.0.9.zip"
+    sha256 "4d1b810aa0ed773f81dceda2cc7b403d01057458730e309856356d4ef4188438"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/cc/85/319a8a684e8ac6d87a1193090e06b6bbb302717496380e225ee10487c888/certifi-2022.6.15.tar.gz"
+    sha256 "84c85a9078b11105f04f3036a9482ae10e4621616db313fe045dd24743a0820d"
+  end
+
   resource "mutagen" do
     url "https://files.pythonhosted.org/packages/f3/d9/2232a4cb9a98e2d2501f7e58d193bc49c956ef23756d7423ba1bd87e386d/mutagen-1.45.1.tar.gz"
     sha256 "6397602efb3c2d7baebd2166ed85731ae1c1d475abca22090b7141ff5034b3e1"
   end
 
   resource "pycryptodomex" do
-    url "https://files.pythonhosted.org/packages/a6/b3/a5e59cd3ad65d4dc470a3a63381d0495885cf1ac7659c83c6bc9e9e79df6/pycryptodomex-3.14.0.tar.gz"
-    sha256 "2d8bda8f949b79b78b293706aa7fc1e5c171c62661252bfdd5d12c70acd03282"
+    url "https://files.pythonhosted.org/packages/52/0d/6cc95a83f6961a1ca041798d222240890af79b381e97eda3b9b538dba16f/pycryptodomex-3.15.0.tar.gz"
+    sha256 "7341f1bb2dadb0d1a0047f34c3a58208a92423cdbd3244d998e4b28df5eac0ed"
   end
 
   resource "websockets" do
-    url "https://files.pythonhosted.org/packages/69/77/591bbc51a5ed6a906a7813e60a9627f988f9546513fcf9d250eb31ec8689/websockets-10.1.tar.gz"
-    sha256 "181d2b25de5a437b36aefedaf006ecb6fa3aa1328ec0236cdde15f32f9d3ff6d"
+    url "https://files.pythonhosted.org/packages/f8/a3/622d9acbfb9a71144b5d7609906bc648c62e3ca5fdbb1c8cca222949d82c/websockets-10.3.tar.gz"
+    sha256 "fc06cc8073c8e87072138ba1e431300e2d408f054b27047d047b549455066ff4"
   end
 
   def install
-    if build.head?
-      system "make", "all", "PREFIX=#{prefix}"
-      bin.install "yt-dlp"
-      man1.install "yt-dlp.1"
-      bash_completion.install "completions/bash/yt-dlp"
-      zsh_completion.install "completions/zsh/_yt-dlp"
-      fish_completion.install "completions/fish/yt-dlp.fish"
-    else
-      virtualenv_install_with_resources
-      man1.install_symlink libexec/"share/man/man1/yt-dlp.1"
-      bash_completion.install libexec/"share/bash-completion/completions/yt-dlp"
-      zsh_completion.install libexec/"share/zsh/site-functions/_yt-dlp"
-      fish_completion.install libexec/"share/fish/vendor_completions.d/yt-dlp.fish"
-    end
+    system "make", "all" if build.head?
+    virtualenv_install_with_resources
+    man1.install_symlink libexec/"share/man/man1/yt-dlp.1"
+    bash_completion.install libexec/"share/bash-completion/completions/yt-dlp"
+    zsh_completion.install libexec/"share/zsh/site-functions/_yt-dlp"
+    fish_completion.install libexec/"share/fish/vendor_completions.d/yt-dlp.fish"
   end
 
   test do
