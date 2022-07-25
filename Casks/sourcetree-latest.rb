72e03d17068bf99e063a45600c16f6d01b409cb6
Marcus Kannicht <27237080+kannicht@users.noreply.github.com>
2022-07-25 12:41:11 +0200

    Update sourcetree-latest.rb

--- Casks/sourcetree-latest.rb
+++ Casks/sourcetree-latest.rb
@@ -7,7 +7,7 @@ cask 'sourcetree-latest' do
   name 'Atlassian Sourcetree'
   homepage 'https://www.sourcetreeapp.com/'
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: '>= :mojave'
 
   app 'Sourcetree.app'
   binary "#{appdir}/Sourcetree.app/Contents/Resources/stree"
