f9482e0b18c800c800081e9ab1bb0968913606ab
sidneys <sidneys.github.io@outlook.com>
2022-09-03 11:09:47 +0200

    chntpw: style fixes

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -1,9 +1,9 @@
 class Chntpw < Formula
-  desc "The Offline NT Password Editor"
+  desc "Offline NT Password Editor"
   homepage "https://github.com/Tody-Guo/chntpw"
   url "https://github.com/sidneys/chntpw/archive/0.99.6.tar.gz"
   sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
-  head "https://github.com/sidneys/chntpw.git"
+  head "https://github.com/sidneys/chntpw.git", branch: "master"
 
   depends_on "sidneys/homebrew/openssl@1.0"
 
