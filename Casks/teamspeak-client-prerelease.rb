426a9f0e23f4127fcb4683fe3a6b7c6f3b938b31
sidneys <sidneys.github.io@outlook.com>
2023-04-03 01:09:47 +0200

    teamspeak-client-prerelease 5.0.0-beta73

--- Casks/teamspeak-client-prerelease.rb
+++ Casks/teamspeak-client-prerelease.rb
@@ -1,17 +1,16 @@
 cask "teamspeak-client-prerelease" do
-  version "5.0.0-beta67"
-  sha256 "090b778fa2d28aa9b319d6a1c231ee64ff6be5c7afc9162d4f2a086aec6fb06f"
+  version "5.0.0-beta73"
+  sha256 "3618128db73e650d0d83f59981fb55a553bf0b941b2fc37f277afb57c30f750c"
 
   url "https://files.teamspeak-services.com/pre_releases/client/#{version}/teamspeak-client.dmg",
       verified: "files.teamspeak-services.com/"
+  name "TeamSpeak Prerelease"
+  desc "Voice communication client"
+  homepage "https://www.teamspeak.com/"
 
   livecheck do
     skip "No version information available"
   end
-
-  name "TeamSpeak Prerelease"
-  desc "Voice communication client"
-  homepage "https://www.teamspeak.com/"
 
   depends_on macos: ">= :sierra"
 
