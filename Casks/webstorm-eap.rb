a5470f71f497ec9b80d7c82dea1bb6ac9e35f458
sidneys <sidneys.github.io@outlook.com>
2024-03-21 18:08:09 +0100

    update: webstorm-eap

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,26 +1,37 @@
-cask 'webstorm-eap' do
-  version '2020.1,201.4515.27'
-  sha256 'ef7a03863b20827d27159a7c42acdb4efed698e2880ae0e0f9d4fe062aa0e559'
+cask "webstorm-eap" do
+  arch arm: "-aarch64"
 
-  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.after_comma}.dmg"
-  appcast 'https://data.services.jetbrains.com/products/releases?code=WS&latest=true&type=eap'
-  name 'WebStorm EAP'
-  homepage 'https://www.jetbrains.com/webstorm/eap/'
+  version "2024.1,241.14494.109"
+  sha256 arm:   "dcf4403b004af10cf3f30dea11ba4bdad8bfc456192fbeb0a5e3ede38bfd51d9",
+         intel: "19872163b90a2fc6addbbdc5baf1e57f33c253b64c3490e6ce64122624970be4"
+
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.after_comma}#{arch}.dmg"
+  name "WebStorm EAP"
+  desc "JavaScript IDE"
+  homepage "https://www.jetbrains.com/webstorm/eap/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=WS&latest=true&type=eap"
+    strategy :json do |json|
+      json["WS"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
 
   auto_updates true
+  depends_on macos: ">= :high_sierra"
 
   app "WebStorm #{version.before_comma} EAP.app"
-
-  uninstall_postflight do
-    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'wstorm') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
-  end
+  binary "#{appdir}/WebStorm #{version.before_comma} EAP.app/Contents/MacOS/webstorm"
 
   zap trash: [
                "~/Library/Application Support/WebStorm#{version.major_minor}",
                "~/Library/Caches/WebStorm#{version.major_minor}",
                "~/Library/Logs/WebStorm#{version.major_minor}",
+               "~/Library/Preferences/com.jetbrains.WebStorm-EAP.plist",
+               "~/Library/Preferences/jetbrains.webstorm.*.plist",
                "~/Library/Preferences/WebStorm#{version.major_minor}",
-               '~/Library/Preferences/com.jetbrains.WebStorm-EAP.plist',
-               '~/Library/Saved Application State/com.jetbrains.WebStorm-EAP.savedState',
+               "~/Library/Saved Application State/com.jetbrains.WebStorm-EAP.savedState",
              ]
 end
